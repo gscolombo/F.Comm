2dff87ac42361fb55af682e72fe347c9c22aa7d1
Gabriel <gabriel.souza.colombo13@gmail.com>
2021-04-06 12:34:37 -0300

    Animaçoes do menu finalizadas

--- js/modules/dinamicScroll.js
+++ js/modules/dinamicScroll.js
@@ -3,17 +3,47 @@ import AnimateOnScroll from './animateOnScroll.js';
 export default class DinamicScroll extends AnimateOnScroll {
     changeMenuBg() {
         const logoContainer = document.querySelector('.logo-container');
-        this.getOffsetDistance();
-        if (pageYOffset >= this.offset[1].offset * 1.667) {
+        const menu = document.querySelector('.menu');
+        if (pageYOffset >= this.offset[1].offset) {
             logoContainer.classList.add('bg-set');
+            menu.classList.add('bg-set');
         } else {
             logoContainer.classList.remove('bg-set');
+            menu.classList.remove('bg-set');
         }
     }
 
+    pointSection(){
+        const sectionArr = [];
+        this.offset.forEach(obj => {
+            if (!(pageYOffset < obj.offset + (innerHeight * 0.3))){
+                sectionArr.push(obj.section.classList.value);
+            }
+        })
+        const lastSection = sectionArr[sectionArr.length - 1];
+        const selector = lastSection + '-opt';
+        this.options.forEach(option => {
+            if (option.classList.contains(selector)) {
+                option.classList.add('visible');
+            } else {
+                option.classList.remove('visible');
+            }
+        });
+    }
+
+    startFunctions(){
+        this.changeMenuBg();
+        this.pointSection();
+        document.querySelector('.menu').classList.remove('active');
+    }
+
     init(){
-        this.changeMenuBg = this.changeMenuBg.bind(this);
-        window.addEventListener('scroll', this.changeMenuBg);
+        this.getOffsetDistance();
+        this.startFunctions = this.startFunctions.bind(this);
+        this.options = document.querySelectorAll('.menu nav li a');
+        this.options[0].classList.add('visible');
+
+        window.addEventListener('scroll', this.startFunctions);
         return this;
     }
 }
--- js/modules/menuControl.js
+++ js/modules/menuControl.js
@@ -1,13 +1,14 @@
 import outclick from './outclick.js';
 
 export default function menuControl() {
+    const sections = document.querySelectorAll('section');
     const menu = document.querySelector('.menu');
     const sandwich = document.querySelector('.sandwich');
     const closeBtn = document.querySelector('.close');
     const options = document.querySelectorAll('.menu li');
     const events = ['click', 'touchstart'];
     
-    function openMenu(e) {
+    function openMenu() {
         menu.classList.add('active');
         outclick('.menu', closeMenu, events);
     }
@@ -16,10 +17,41 @@ export default function menuControl() {
         menu.classList.remove('active');
     }
     
-    
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const link = event.currentTarget;
+        sections.forEach(section => {
+           if (link.querySelector(`[class^=${section.getAttribute('id')}]`) !== null){
+                const sectionHeader = section.querySelector('header');
+                if (sectionHeader !== null) {
+                    if (!section.classList.contains('services')) {
+                        scrollTo({
+                            top: sectionHeader.offsetTop - 120,
+                            left: 0,
+                            behavior: 'smooth',
+                        });
+                    } else {
+                        scrollTo({
+                            top: sectionHeader.offsetTop - 20,
+                            left: 0,
+                            behavior: 'smooth',
+                        });
+                    } 
+                } else {
+                    scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                }
+            }
+        })
+    }
+
     events.forEach(event => {
         sandwich.addEventListener(event, openMenu);
         closeBtn.addEventListener(event, closeMenu);
-        options.forEach(option => option.addEventListener(event, closeMenu));
+        options.forEach(option => option.addEventListener(event, scrollToSection));
     });
 }
